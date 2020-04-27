PixelExperience/device_qcom_sepolicy-legacy-um

sepolicy: Include wifi-ext sepol
Change-Id: Idf06388880d0b79a0fadb290129ec97999e19879

sepolicy.mk
@@ -28,3 +28,4 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy-legacy-um/private
 
 -include device/custom/sepolicy/qcom/sepolicy.mk
+-include device/custom/sepolicy/wifi-ext/sepolicy.mk

AICP/device_qcom_sepolicy

sepolicy.mk
@@ -59,3 +59,4 @@ ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
 endif
 
 -include device/aicp/sepolicy/qcom/sepolicy.mk
+-include device/aicp/sepolicy/wifi-ext/sepolicy.mk